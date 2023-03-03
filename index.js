48bcc382d1290223572dfb70b1d5302b74f9f135
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-03-03 14:30:29 +0530

    api initialized and hotel api made

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("connected to mongoDb");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongodb disconnected");
+});
+
+//middlewares
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.listen(8800, () => {
+  connect();
+  console.log("connected to backend");
+});
