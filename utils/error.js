1a1d2a6b081a68afb6e29af8d0c9a81bff654af1
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-03-03 17:11:20 +0530

    authentication api made

--- utils/error.js
+++ utils/error.js
@@ -1,4 +1,4 @@
-export const createError = (message, status) => {
+export const createError = (status, message) => {
   const error = new Error();
   error.status = status;
   error.message = message;
